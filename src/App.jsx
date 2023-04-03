27873f73aa8bdbc22b82e093da953a096d1c8079
Mampi Das <66109177+letsbecreative@users.noreply.github.com>
2023-04-03 20:42:07 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+// import Card from './Cards';
+// import Sdata from './Sdata';
+import Netflix from "./Netflix";
+import Amazon from "./Amazon";
+
+const favSeries = "netflix";
+
+// const FavS = () => {
+//     if(favSeries === "netflix") {
+//         return <Netflix />;
+//     } else {
+//         return <Amazon />;
+//     }
+// };
+
+const App = () => ( 
+    <>
+        <h1 className="heading_style"> List of top 5 Netflix Series in 2020 </h1>
+        {/* <FavS /> */}
+
+        { favSeries === "netflix" ? <Netflix /> : <Amazon /> }
+    </> 
+);
+
+export default App;
---
+++ src/Sdata.jsx
@@ -0,0 +1,43 @@
+const Sdata = [
+    {
+        id: 1,
+        imgscr: "https://wallpapercave.com/wp/wp4056410.jpg",  
+        title: " A Netflix Original Series ",
+        sname: "DARK",
+        link: "https://www.netflix.com/in/title/80990668?source=35", 
+    },
+
+    {
+        id: 2,
+        imgscr: "https://wallpaperaccess.com/full/4600594.jpg",  
+        title: " A Netflix Original Series ",
+        sname :  "EXTRA CURRICULAR",
+        link :  "https://www.netflix.com/in/title/80990668?source=35",
+    },
+
+    {
+        id: 3,
+        imgscr: "https://wallpapercave.com/wp/wp1917154.jpg",  
+        title: " A Netflix Original Series ",
+        sname :  "Stranger Things",
+        link :  "https://www.netflix.com/in/title/80990668?source=35",
+    },
+
+    {
+        id: 4,
+        imgscr: "https://images.justwatch.com/poster/8589251/s592",  
+        title: " azon Original Series ",
+        sname :  "the Vampire Diaries",
+        link :  "https://www.netflix.com/in/title/80990668?source=35",
+    },
+
+    {
+        id: 5,
+        imgscr: "https://i.pinimg.com/474x/7b/19/58/7b19584ec5ffba063a121508e78fe7a4.jpg",  
+        title: " Netflix Original Series ",
+        sname :  "My First-2 Love",
+        link :  "https://www.netflix.com/in/title/81026700?source=35",
+    },
+];
+
+export default Sdata;
